a5d8dccc438d312f1672adbdd24177c6a8d4d073
Zul Ikram Musaddik Rayat <rayathossain49@gmail.com>
2025-08-06 03:48:40 +0600

    feat: switch database client to Neon and update drizzle ORM integration

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,9 +1,9 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
 // Disable prefetch as it's not supported by all postgres providers
-const client = postgres(process.env.DATABASE_URL!, { prepare: false });
-export const db = drizzle(client, { schema });
-
+const client = neon(process.env.DATABASE_URL!, {  });
+export const db = drizzle({ client, schema });
+ 
 export * from './schema';
