c16ce4576c31b82eee3a1cc7f95cad907244cad3
Zul Ikram Musaddik Rayat <rayathossain49@gmail.com>
2025-08-06 19:30:52 +0600

    feat: add postgres and biome dependencies; update drizzle configuration for improved database handling

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,9 +1,34 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema';
+import { neon } from "@neondatabase/serverless";
+import postgres from "postgres";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/postgres-js";
+import * as schema from "./schema";
+
+let db:
+	| ReturnType<typeof drizzlePg<typeof schema>>
+	| ReturnType<typeof drizzleNeon<typeof schema>>;
+
+declare global {
+	var drizzle: typeof db;
+	var client: ReturnType<typeof postgres>;
+}
 
 // Disable prefetch as it's not supported by all postgres providers
-const client = neon(process.env.DATABASE_URL!, {  });
-export const db = drizzle({ client, schema });
- 
-export * from './schema';
+if (process.env.NODE_ENV === "production") {
+	const client = neon(process.env.DATABASE_URL!, {});
+	db = drizzleNeon({ client, schema });
+} else {
+	const client =
+		globalThis.client ||
+		postgres(process.env.DATABASE_URL!, {
+			max: 1,
+			idle_timeout: 30,
+		});
+	globalThis.client = client;
+	db = globalThis.drizzle || drizzlePg({ client, schema });
+	globalThis.drizzle = db;
+}
+
+export { db };
+
+export * from "./schema";
